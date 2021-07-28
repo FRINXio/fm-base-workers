8a0c2e9ace66b88bf1b92bb309ee7199c5b4247a
Jozef Volak <jvolak@frinx.io>
2021-07-28 12:21:07 +0200

    Update workers for Uniconfig 4.2.7

--- frinx_conductor_workers/netconf_worker.py
+++ frinx_conductor_workers/netconf_worker.py
@@ -2,84 +2,196 @@ from __future__ import print_function
 
 import copy
 import json
-from string import Template
-
 import requests
+import logging
 
-from frinx_conductor_workers.frinx_rest import uniconfig_url_base, additional_uniconfig_request_params, parse_response, elastic_url_base, elastic_headers, add_uniconfig_tx_cookie
+from string import Template
+from frinx_conductor_workers.frinx_rest import (
+    uniconfig_url_base,
+    additional_uniconfig_request_params,
+    parse_response,
+    elastic_url_base,
+    elastic_headers,
+    add_uniconfig_tx_cookie
+)
+
+
+
+local_logs = logging.getLogger(__name__)
 
 uniconfig_url_netconf_mount = uniconfig_url_base + "/data/network-topology:network-topology/topology=topology-netconf/node=$id"
+uniconfig_url_netconf_mount_sync = uniconfig_url_base + "/operations/connection-manager:install-node"
+uniconfig_url_netconf_unmount_sync = uniconfig_url_base + "/operations/connection-manager:uninstall-node"
 uniconfig_url_netconf_mount_oper = uniconfig_url_base + "/data/network-topology:network-topology/topology=topology-netconf/node=$id?content=nonconfig"
 
-mount_template = {
-    "node": 
-        [{
-            "node-id": "",
-
+sync_mount_template = {
+    "input":
+    {
+        "node-id": "",
+        "netconf":
+        {
             "netconf-node-topology:host": "",
-            "netconf-node-topology:port": "",
-            "netconf-node-topology:keepalive-delay": "",
-            "netconf-node-topology:tcp-only": "",
+            "netconf-node-topology:port": 2022,
+            "netconf-node-topology:keepalive-delay": 5,
+            "netconf-node-topology:max-connection-attempts": 1,
+            "netconf-node-topology:connection-timeout-millis": 60000,
+            "netconf-node-topology:default-request-timeout-millis": 60000,
+            "netconf-node-topology:tcp-only": False,
             "netconf-node-topology:username": "",
             "netconf-node-topology:password": "",
-        }]
+            "netconf-node-topology:sleep-factor": 1.0,
+            "uniconfig-config:uniconfig-native-enabled": True,
+            "netconf-node-topology:edit-config-test-option": "set",
+            "uniconfig-config:blacklist":
+            {
+                "extension":
+                [
+                    "tailf:display-when false"
+                ]
+            }
+        }
+    }
 }
 
 
+
 def execute_mount_netconf(task):
-    device_id = task['inputData']['device_id']
-    uniconfig_tx_id = task['inputData']['uniconfig_tx_id'] if 'uniconfig_tx_id' in task['inputData'] else ""
+    """
+    Build a template for netconf mounting body after parsing input device
+    parameters and send a PUT request to Uniconfig. These requests can be
+    viewed and tested also in postman collections for each device.
 
-    mount_body = copy.deepcopy(mount_template)
+    Args:
+        task: A dict with a complete device data and optional uniconfig_tx_id
+    Returns:
+        response: dict, e.g. {'status': 'COMPLETED', 'output': {'url': id_url,
+                                                  'request_body': mount_body,
+                                                  'response_code': 200,
+                                                  'response_body': response_json},
+                }}
+    """
+    # First, check the validity of the provided IP address.
 
-    mount_body["node"][0]["node-id"] = task['inputData']['device_id']
-    mount_body["node"][0]["netconf-node-topology:host"] = task['inputData']['host']
-    mount_body["node"][0]["netconf-node-topology:port"] = task['inputData']['port']
-    mount_body["node"][0]["netconf-node-topology:keepalive-delay"] = task['inputData']['keepalive-delay']
-    mount_body["node"][0]["netconf-node-topology:tcp-only"] = task['inputData']['tcp-only']
-    mount_body["node"][0]["netconf-node-topology:username"] = task['inputData']['username']
-    mount_body["node"][0]["netconf-node-topology:password"] = task['inputData']['password']
+    device_id = task["inputData"]["device_id"]
+    uniconfig_tx_id = (
+        task["inputData"]["uniconfig_tx_id"]
+        if "uniconfig_tx_id" in task["inputData"]
+        else ""
+    )
 
-    if 'reconcile' in task['inputData'] and task['inputData']['reconcile'] is not None\
-            and task['inputData']['reconcile'] is not"":
-        mount_body["node"][0]["node-extension:reconcile"] = task['inputData']['reconcile']
+    mount_body = copy.deepcopy(sync_mount_template)
+    mount_body["input"]["node-id"] = task["inputData"]["device_id"]
+    mount_body["input"]["netconf"]["netconf-node-topology:host"] = task["inputData"]["host"]
+    mount_body["input"]["netconf"]["netconf-node-topology:port"] = task["inputData"]["port"]
+    mount_body["input"]["netconf"]["netconf-node-topology:keepalive-delay"] = task["inputData"]["keepalive-delay"]
+    mount_body["input"]["netconf"]["netconf-node-topology:tcp-only"] = task["inputData"]["tcp-only"]
+    mount_body["input"]["netconf"]["netconf-node-topology:username"] = task["inputData"]["username"]
+    mount_body["input"]["netconf"]["netconf-node-topology:password"] = task["inputData"]["password"]
 
-    if 'sleep-factor' in task['inputData'] and task['inputData']['sleep-factor'] is not None\
-            and task['inputData']['sleep-factor'] is not "":
-        mount_body["node"][0]["netconf-node-topology:sleep-factor"] = task['inputData']['sleep-factor']
+    if (
+        "reconcile" in task["inputData"]
+        and task["inputData"]["reconcile"] is not None
+        and task["inputData"]["reconcile"] is not ""
+    ):
+        mount_body["input"]["netconf"]["node-extension:reconcile"] = task["inputData"]["reconcile"]
 
-    if 'between-attempts-timeout-millis' in task['inputData'] and task['inputData']['between-attempts-timeout-millis'] is not None\
-            and task['inputData']['between-attempts-timeout-millis'] is not "":
-        mount_body["node"][0]["netconf-node-topology:between-attempts-timeout-millis"] = task['inputData']['between-attempts-timeout-millis']
+    if (
+        "schema-cache-directory" in task["inputData"]
+        and task["inputData"]["schema-cache-directory"] is not None
+        and task["inputData"]["schema-cache-directory"] is not ""
+    ):
+        mount_body["input"]["netconf"]["netconf-node-topology:schema-cache-directory"] = task["inputData"][
+            "schema-cache-directory"]
 
-    if 'connection-timeout-millis' in task['inputData'] and task['inputData']['connection-timeout-millis'] is not None\
-            and task['inputData']['connection-timeout-millis'] is not "":
-        mount_body["node"][0]["netconf-node-topology:connection-timeout-millis"] = task['inputData']['connection-timeout-millis']
+    if (
+        "sleep-factor" in task["inputData"]
+        and task["inputData"]["sleep-factor"] is not None
+        and task["inputData"]["sleep-factor"] is not ""
+    ):
+        mount_body["input"]["netconf"]["netconf-node-topology:sleep-factor"] = task["inputData"][
+            "sleep-factor"
+        ]
 
-    if 'uniconfig-native' in task['inputData'] and task['inputData']['uniconfig-native'] is not None\
-            and task['inputData']['uniconfig-native'] is not "":
-        mount_body["node"][0]["uniconfig-config:uniconfig-native-enabled"] = task['inputData']['uniconfig-native']
+    if (
+        "between-attempts-timeout-millis" in task["inputData"]
+        and task["inputData"]["between-attempts-timeout-millis"] is not None
+        and task["inputData"]["between-attempts-timeout-millis"] is not ""
+    ):
+        mount_body["input"]["netconf"][
+            "netconf-node-topology:between-attempts-timeout-millis"
+        ] = task["inputData"]["between-attempts-timeout-millis"]
 
-    if 'blacklist' in task['inputData'] and task['inputData']['blacklist'] is not None:
-        mount_body["node"][0]["uniconfig-config:blacklist"] = {'uniconfig-config:path':[]}
-        model_array = [model.strip() for model in task['inputData']['blacklist'].split(',')]
+    if (
+        "connection-timeout-millis" in task["inputData"]
+        and task["inputData"]["connection-timeout-millis"] is not None
+        and task["inputData"]["connection-timeout-millis"] is not ""
+    ):
+        mount_body["input"]["netconf"]["netconf-node-topology:connection-timeout-millis"] = task[
+            "inputData"
+        ]["connection-timeout-millis"]
+
+    if (
+        "uniconfig-native" in task["inputData"]
+        and task["inputData"]["uniconfig-native"] is not None
+        and task["inputData"]["uniconfig-native"] is not ""
+    ):
+        mount_body["input"]["netconf"]["uniconfig-config:uniconfig-native-enabled"] = task[
+            "inputData"
+        ]["uniconfig-native"]
+
+    if "blacklist" in task["inputData"] and task["inputData"]["blacklist"] is not None:
+        mount_body["input"]["netconf"]["uniconfig-config:blacklist"] = {"uniconfig-config:path": []}
+        model_array = [
+            model.strip() for model in task["inputData"]["blacklist"].split(",")
+        ]
         for model in model_array:
-            mount_body["node"][0]["uniconfig-config:blacklist"]["uniconfig-config:path"].append(model)
-            
-    if 'dry-run-journal-size' in task['inputData'] and task['inputData']['dry-run-journal-size'] is not None:
-        mount_body["node"][0]["netconf-node-topology:dry-run-journal-size"] = task['inputData']['dry-run-journal-size']
+            mount_body["input"]["netconf"]["uniconfig-config:blacklist"][
+                "uniconfig-config:path"
+            ].append(model)
 
-    id_url = Template(uniconfig_url_netconf_mount).substitute({"id": device_id})
+    if (
+        "dry-run-journal-size" in task["inputData"]
+        and task["inputData"]["dry-run-journal-size"] is not None
+    ):
+        mount_body["input"]["netconf"]["netconf-node-topology:dry-run-journal-size"] = task[
+            "inputData"
+        ]["dry-run-journal-size"]
 
-    r = requests.put(id_url, data=json.dumps(mount_body), headers=add_uniconfig_tx_cookie(uniconfig_tx_id), **additional_uniconfig_request_params)
+    if (
+        "enabled-notifications" in task["inputData"]
+        and task["inputData"]["enabled-notifications"] is not None
+    ):
+        mount_body["input"]["netconf"]["netconf-node-topology:enabled-notifications"] = task[
+            "inputData"
+        ]["enabled-notifications"]
+
+    if 'capability' in task["inputData"]:
+        mount_body["input"]["netconf"]["netconf-node-topology:yang-module-capabilities"] = {"capability": []}
+        mount_body["input"]["netconf"]["netconf-node-topology:yang-module-capabilities"]["capability"].append(task[
+            "inputData"
+        ]["capability"])
+
+    id_url = uniconfig_url_netconf_mount_sync
+
+    r = requests.post(id_url, data=json.dumps(mount_body),
+                     headers=add_uniconfig_tx_cookie(uniconfig_tx_id),
+                     timeout=600,
+                     **additional_uniconfig_request_params)
     response_code, response_json = parse_response(r)
 
-    if response_code == requests.codes.created or response_code == requests.codes.no_content:
+    error_message_for_already_installed = "Node has already been installed using NETCONF protocol"
+
+    failed = response_json.get("output", {}).get("status") == "fail"
+    already_installed = response_json.get("output", {}).get(
+        "error-message") == error_message_for_already_installed
+
+    if not failed or already_installed:
         return {'status': 'COMPLETED', 'output': {'url': id_url,
                                                   'request_body': mount_body,
                                                   'response_code': response_code,
                                                   'response_body': response_json},
                 'logs': ["Mountpoint with ID %s registered" % device_id]}
+
     else:
         return {'status': 'FAILED', 'output': {'url': id_url,
                                                'request_body': mount_body,
@@ -92,9 +204,19 @@ def execute_unmount_netconf(task):
     device_id = task['inputData']['device_id']
     uniconfig_tx_id = task['inputData']['uniconfig_tx_id'] if 'uniconfig_tx_id' in task['inputData'] else ""
 
-    id_url = Template(uniconfig_url_netconf_mount).substitute({"id": device_id})
+    id_url = Template(uniconfig_url_netconf_mount_sync).substitute(
+        {"id": device_id}
+    )
 
-    r = requests.delete(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id), **additional_uniconfig_request_params)
+    unmount_body = { "input":
+            {
+                "node-id": device_id,
+                "connection-type":"netconf"
+            }}
+    r = requests.post(id_url,
+                        data=json.dumps(unmount_body),
+                        headers=add_uniconfig_tx_cookie(uniconfig_tx_id),
+                        **additional_uniconfig_request_params)
     response_code, response_json = parse_response(r)
 
     return {'status': 'COMPLETED', 'output': {'url': id_url,
@@ -102,36 +224,14 @@ def execute_unmount_netconf(task):
                                               'response_body': response_json},
             'logs': ["Mountpoint with ID %s removed" % device_id]}
 
-
-def execute_check_netconf_id_available(task):
-    device_id = task['inputData']['device_id']
-    uniconfig_tx_id = task['inputData']['uniconfig_tx_id'] if 'uniconfig_tx_id' in task['inputData'] else ""
-
-    id_url = Template(uniconfig_url_netconf_mount).substitute({"id": device_id}) + "?content=config"
-
-    r = requests.get(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id), **additional_uniconfig_request_params)
-    response_code, response_json = parse_response(r)
-
-    if response_code != requests.codes.not_found:
-        # Mountpoint with such ID already exists
-        return {'status': 'FAILED', 'output': {'url': id_url,
-                                               'response_code': response_code,
-                                               'response_body': response_json},
-                'logs': ["Unable to mount device with ID %s" % device_id]}
-    else:
-        return {'status': 'COMPLETED', 'output': {'url': id_url,
-                                                  'response_code': response_code,
-                                                  'response_body': response_json},
-                'logs': ["Mountpoint with ID %s is available" % device_id]}
-
-
 def execute_check_connected_netconf(task):
     device_id = task['inputData']['device_id']
     uniconfig_tx_id = task['inputData']['uniconfig_tx_id'] if 'uniconfig_tx_id' in task['inputData'] else ""
 
     id_url = Template(uniconfig_url_netconf_mount_oper).substitute({"id": device_id})
 
-    r = requests.get(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id), **additional_uniconfig_request_params)
+    r = requests.get(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id),
+                     **additional_uniconfig_request_params)
     response_code, response_json = parse_response(r)
 
     if response_code == requests.codes.ok and response_json["node"][0]["netconf-node-topology:connection-status"] == "connected":
@@ -151,9 +251,12 @@ def read_structured_data(task):
     uri = task['inputData']['uri']
     uniconfig_tx_id = task['inputData']['uniconfig_tx_id'] if 'uniconfig_tx_id' in task['inputData'] else ""
 
-    id_url = Template(uniconfig_url_netconf_mount).substitute({"id": device_id}) + "/yang-ext:mount" + (uri if uri else "")
+    id_url = Template(uniconfig_url_netconf_mount).substitute(
+        {"id": device_id, "base_url": task['inputData']['uniconfig_url_base']}
+    ) + "/yang-ext:mount" + (uri if uri else "")
 
-    r = requests.get(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id), **additional_uniconfig_request_params)
+    r = requests.get(id_url, headers=add_uniconfig_tx_cookie(uniconfig_tx_id),
+                     **additional_uniconfig_request_params)
     response_code, response_json = parse_response(r)
 
     if response_code == requests.codes.ok:
@@ -168,19 +271,20 @@ def read_structured_data(task):
                 'logs': ["Unable to read device with ID %s" % device_id]}
 
 
+
 def start(cc):
-    print('Starting Netconf workers')
+    local_logs.info("Starting Netconf workers")
 
     cc.register('Netconf_mount_netconf', {
         "name": "Netconf_mount_netconf",
         "description": "{\"description\": \"mount a Netconf device\", \"labels\": [\"BASICS\",\"NETCONF\"]}",
         "retryCount": 0,
         "ownerEmail":"example@example.com",
-        "timeoutSeconds": 60,
+        "timeoutSeconds": 600,
         "timeoutPolicy": "TIME_OUT_WF",
         "retryLogic": "FIXED",
         "retryDelaySeconds": 0,
-        "responseTimeoutSeconds": 10,
+        "responseTimeoutSeconds": 600,
         "inputKeys": [
             "device_id",
             "host",
@@ -205,7 +309,7 @@ def start(cc):
             "response_body"
         ]
     })
-    cc.start('Netconf_mount_netconf', execute_mount_netconf, False)
+    cc.start('Netconf_mount_netconf', execute_mount_netconf, False, limit_to_thread_count=None)
 
     cc.register('Netconf_unmount_netconf', {
         "name": "Netconf_unmount_netconf",
@@ -227,50 +331,7 @@ def start(cc):
             "response_body"
         ]
     })
-    cc.start('Netconf_unmount_netconf', execute_unmount_netconf, False)
-
-    cc.register('Netconf_check_netconf_connected', {
-        "name": "Netconf_check_netconf_connected",
-        "retryCount": 20,
-        "ownerEmail":"example@example.com",
-        "timeoutSeconds": 10,
-        "timeoutPolicy": "TIME_OUT_WF",
-        "retryLogic": "FIXED",
-        "retryDelaySeconds": 5,
-        "responseTimeoutSeconds": 10,
-        "inputKeys": [
-            "device_id",
-            "uniconfig_tx_id"
-        ],
-        "outputKeys": [
-            "url",
-            "response_code",
-            "response_body"
-        ]
-    })
-    cc.start('Netconf_check_netconf_connected', execute_check_connected_netconf, False)
-
-    cc.register('Netconf_check_netconf_id_available', {
-        "name": "Netconf_check_netconf_id_available",
-        "description": "{\"description\": \"checks if netconf device is available\", \"labels\": [\"BASICS\",\"NETCONF\"]}",
-        "retryCount": 0,
-        "ownerEmail":"example@example.com",
-        "timeoutSeconds": 60,
-        "timeoutPolicy": "TIME_OUT_WF",
-        "retryLogic": "FIXED",
-        "retryDelaySeconds": 0,
-        "responseTimeoutSeconds": 10,
-        "inputKeys": [
-            "device_id",
-            "uniconfig_tx_id"
-        ],
-        "outputKeys": [
-            "url",
-            "response_code",
-            "response_body"
-        ]
-    })
-    cc.start('Netconf_check_netconf_id_available', execute_check_netconf_id_available, False)
+    cc.start('Netconf_unmount_netconf', execute_unmount_netconf, False, limit_to_thread_count=None)
 
     cc.register('Netconf_read_structured_device_data', {
         "name": "Netconf_read_structured_device_data",
