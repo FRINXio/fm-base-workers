bffa98883b5883fbc7e30075ef3589dbe28df9b6
Jozef Volak <jvolak@frinx.io>
2021-07-28 12:21:07 +0200

    Update workers for Uniconfig 4.2.7

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
-import time
 import os
+import time
+import json
+import logging.config
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 from conductor.FrinxConductorWrapper import FrinxConductorWrapper
 from frinx_conductor_workers.frinx_rest import conductor_url_base, conductor_headers
-from frinx-conductor-workers import cli_worker
-from frinx-conductor-workers import netconf_worker
-from frinx-conductor-workers import uniconfig_worker
-from frinx-conductor-workers import common_worker
-from frinx-conductor-workers import http_worker
-from frinx-conductor-workers.import_workflows import import_workflows
+from frinx_conductor_workers import import_workflows
+
+from frinx_conductor_workers import cli_worker
+from frinx_conductor_workers import netconf_worker
+from frinx_conductor_workers import uniconfig_worker
+from frinx_conductor_workers import common_worker
+from frinx_conductor_workers import http_worker
+from frinx_conductor_workers import import_workflows
+
+log = logging.getLogger(__name__)
 
 workflows_folder_path = './workflows'
 healtchchek_file_path = './healthcheck'
 
+def setup_logging(
+        default_path='logging-config.json',
+        default_level=logging.INFO,
+        env_key='LOG_CFG'
+):
+    """Setup logging configuration
+    """
+    path = os.path.join(os.path.dirname(__file__), default_path)
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
 
 def main():
+    setup_logging()
+
     if os.path.exists(healtchchek_file_path):
         os.remove(healtchchek_file_path)
 
     print('Starting FRINX workers')
     cc = FrinxConductorWrapper(conductor_url_base, 1, 1, headers=conductor_headers)
-    cc.start_queue_polling()
     register_workers(cc)
     import_workflows(workflows_folder_path)
 
+    cc.start_workers()
+
+
     with open(healtchchek_file_path, 'w'): pass
-
-
-
 
     # block
     while 1:
