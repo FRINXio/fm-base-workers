cf6ab0a4efafef87944098957cf4a860cc0f7540
Stanislav Chlebec <stanislav.chlebec@polystar.com>
2023-05-22 16:32:41 +0200

    satisfy black formatter

--- system_tests/tests/frinx/test_performance.py
+++ system_tests/tests/frinx/test_performance.py
@@ -110,9 +110,9 @@ async def collect_stats(session, wf_ids: tuple[str]) -> dict:
     duration_quantiles_dict = dict()
     for quantile in duration_quantiles:
         q = q + 10
-        duration_quantiles_dict[ f"quantile{q}_duration_seconds" ] = quantile / 1000
+        duration_quantiles_dict[f"quantile{q}_duration_seconds"] = quantile / 1000
 
-    return  duration_quantiles_dict | {
+    return duration_quantiles_dict | {
         "median_duration_seconds": timedelta(milliseconds=statistics.median(durations)).seconds,
         "average_duration_seconds": timedelta(milliseconds=statistics.mean(durations)).seconds,
         "max_duration_seconds": timedelta(milliseconds=max(durations)).seconds,
